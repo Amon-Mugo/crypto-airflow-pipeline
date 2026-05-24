ed2a39d362e7aefe9431b12b6fd50903fd25a22b
Amon-Mugo <amonkariuki325@gmail.com>
2026-05-24 12:03:55 +0300

    Fix Docker networking - use host mode, install dbt inside Docker

--- dags/crypto_pipeline.py
+++ dags/crypto_pipeline.py
@@ -3,21 +3,21 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'airflow',
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 with DAG(
-    'crypto_pipeline',
+    "crypto_pipeline",
     default_args=default_args,
-    description='Daily crypto dbt pipeline',
-    schedule_interval='0 7 * * *',
+    description="Daily crypto dbt pipeline",
+    schedule_interval="0 7 * * *",
     start_date=datetime(2026, 1, 1),
     catchup=False,
 ) as dag:
 
     run_dbt = BashOperator(
-        task_id='run_dbt_models',
-        bash_command='/home/amon/data_engineering/venv/bin/dbt run --project-dir /home/amon/data_engineering/crypto-analytics/crypto_dbt --profiles-dir /home/amon/.dbt ',
+        task_id="run_dbt_models",
+        bash_command="/home/airflow/.local/bin/dbt run --project-dir /home/amonmugo/PROJECTS/data_engineering/crypto-analytics/crypto_dbt --profiles-dir /home/amonmugo/.dbt ",
     )
